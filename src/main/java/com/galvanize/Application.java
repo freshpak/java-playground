828d52bcabbff4c3b4b9bfb0d04e940fbe0838b7
Edward Pak <edwardpak7@gmail.com>
2022-07-12 12:53:54 -0700

    command line arguments

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,9 +2,15 @@ package com.galvanize;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
-        System.out.println("Hello world!");
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        } else if (args.length == 1) {
+            System.out.println(String.format("Please specify an email for %s", args[0]));
+        } else {
+            System.out.println(String.format("%s <%s>", args[0], args[1]));
+        }
 
     }
 
